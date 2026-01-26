1587972bb45094ebea52fb61bf024fa35ee67af4
Juhyun Son <thswngus94@gmail.com>
2026-01-26 09:57:32 +0900

    Update Obstacle.cs

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -14,4 +14,8 @@ public class Obstacle : MonoBehaviour
         // 회전 적용
         transform.Rotate(0, 0, rotationSpeed * direction * Time.deltaTime);
     }
+
+    /* z축에다 사용하는 이유?
+     * 평면에 놓인 종이(오브젝트)를 평면 위에서 돌리려면, 종이 정중앙에 핀(Z축)을 꽂고 돌려야 하죠? 
+     * 그래서 transform.Rotate(0, 0, angle) 처럼 X, Y값은 0으로 두고 Z값만 변화를 주는 것입니다.*/
 }
